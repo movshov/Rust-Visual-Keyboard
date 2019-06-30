bc7cac2b3548bcb7b7507c80e682c8e8e374974e
Bar Movshovich <barmovshovich@Bars-MacBook-Pro.local>
2019-06-30 00:00:13 -0700

    Changed background color to grey.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use ggez::*;    //tells rust we want to use ggez.
 
 struct State {
-    dt: std::time::Duration,
 }
 
 impl ggez::event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        self.dt = timer::delta(ctx);
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
+        graphics::clear(ctx, graphics::Color::from((0.500, 0.500, 0.500, 0.0)));    //set the background color. In this case grey. 
+        let rect = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(500.0, 250.0, 200.0, 100.0),
+            graphics::WHITE,
+            )?;
+        graphics::draw(ctx, &rect, graphics::DrawParam::default())?;
+        graphics::present(ctx)?;
         Ok(())
     }
 
 }
 fn main(){
-    let state = &mut State { dt: std::time::Duration::new(0, 0) };
+    let state = &mut State { };
 
     //let c = conf::Conf::new();    //default.
 
@@ -24,9 +30,10 @@ fn main(){
     .window_setup(conf::WindowSetup::default().title("Visual Keyboard"))  //set title. 
     //.window_setup(conf::WindowSetup::default().resizable(true));  //...Not sure why not working.
     //.window_mode(conf::WindowMode::default().dimensions(1000.0, 1000.0)); //manually set window size. 
+    .window_mode(conf::WindowMode::default().borderless(false)) //Desktop enum work True doesn't....?
     .window_mode(conf::WindowMode::default().fullscreen_type(ggez::conf::FullscreenType::Desktop)); //Desktop enum work True doesn't....?
 
-    let (ctx, event_loop) = &mut cb.build().unwrap();
 
+    let (ctx, event_loop) = &mut cb.build().unwrap();
     event::run(ctx, event_loop, state).unwrap();
 }
